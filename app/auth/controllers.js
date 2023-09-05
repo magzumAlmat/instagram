fe554f83396d7f3f1f1da1c317ac626f7071220b
magzumAlmat <almat.magzum@gmail.com>
2023-09-05 16:00:20 +0600

    added auth with hashed password

--- app/auth/controllers.js
+++ app/auth/controllers.js
@@ -1,5 +1,5 @@
 const User=require('./User')
-
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const jwtOptions = {
     secretOrKey: "mySecret"
@@ -9,16 +9,14 @@ const authentificateUser=async(req,res)=>{
     console.log('iam in auth user')
     let user = await User.findOne({where: {email: req.body.email}});
 
-    const token = jwt.sign({
-        id: user.id,
-        email: user.email,
-        full_name: user.full_name,
-        phone: user.phone,
-       
-    }, jwtOptions.secretOrKey, {
-        expiresIn: 24 * 60 * 60 * 365
-    });
-    res.status(200).send({token});
+    const {  password } = req.body;
+    
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+    res.status(200).json({ prompt: 'Authorization successful' });
 
 }
 
@@ -49,29 +47,44 @@ const authentificateUser=async(req,res)=>{
 //         expiresIn: 24 * 60 * 60 * 365
 //     });
 // res.status(200).send('User Created',{token});
-const createUser =async(req,res)=>{
-    console.log('iam in create user')
-    
-    let user = await User.findOne({where: {email: req.body.email}});
-    if (!user){
-        await User.create({
-            email:req.body.email,
-            username:req.body.username,
-            password:req.body.password
-        })
-      
+
+    const createUser =async(req,res)=>{
+        console.log('iam in create user')
+        
+        let user = await User.findOne({where: {email: req.body.email}});
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        if (!user){
+            await User.create({
+                email:req.body.email,
+                username:req.body.username,
+                password:hashedPassword
+            })
+            const token = jwt.sign({
+                id: user.id,
+                email: user.email,
+                full_name: user.full_name,
+                phone: user.phone,
+              
+            }, jwtOptions.secretOrKey, {
+                expiresIn: 24 * 60 * 60 * 365
+            });
+            res.status(200).send({token});
+        }else{
+
+        const token = jwt.sign({
+            id: user.id,
+            email: user.email,
+            full_name: user.full_name,
+            phone: user.phone,
+          
+        }, jwtOptions.secretOrKey, {
+            expiresIn: 24 * 60 * 60 * 365
+        });
+        res.status(200).send({token});
     }
-    const token = jwt.sign({
-        id: user.id,
-        email: user.email,
-        full_name: user.full_name,
-        phone: user.phone,
-      
-    }, jwtOptions.secretOrKey, {
-        expiresIn: 24 * 60 * 60 * 365
-    });
-    res.status(200).send({token});
-  
+            // res.status(200).send('User Created');
+           
+        
         // res.status(200).send('User Created');
        
     
