0ed7712d41cff99849677596024c85ffdb36c1c9
magzumAlmat <almat.magzum@gmail.com>
2023-09-13 09:55:39 +0600

    production backend8

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 module.exports={
     development:{
         username:'admin',
@@ -6,6 +7,18 @@ module.exports={
         host:'localhost',
         dialect:'postgres'
     },
-    production:{}
+    production:{
+        username:'doadmin',
+        password:'AVNS_Dna23IDVpWI8ROWtfsj',
+        database:'defaultdb',
+        host:'db-postgresql-sgp1-33347-do-user-11457016-0.b.db.ondigitalocean.com',
+        dialect:'postgres',
+        port:25060,
+        dialectOptions: {
+            ssl: {
+              ca: fs.readFileSync('config/ca-certificate.crt')
+            },
+        },
+    }
 }
 
--- config/db.js
+++ config/db.js
@@ -1,14 +1,33 @@
+const fs = require('fs')
 const{Sequelize} = require('sequelize');
-
+let sequelize;
 const dbConf = require('./config')
+if (process.env.NODE_ENV==='production'){
+    console.log('prod MODE TURNED ON')
+     sequelize = new Sequelize({
+        database:dbConf.production.database, 
+        username:dbConf.production.username,
+        host:dbConf.production.host,
+        dialect:dbConf.production.dialect,
+        password:dbConf.production.password, 
+        port:dbConf.production.port,
+        dialectOptions: {
+            ssl: {
+              ca: fs.readFileSync('config/ca-certificate.crt')
+            },
+        },
 
-const sequelize = new Sequelize({
+    })
+}
+else{
+sequelize = new Sequelize({
     database:dbConf.development.database, 
     username:dbConf.development.username,
     host:dbConf.development.host,
     dialect:dbConf.development.dialect,
     password:dbConf.development.password, 
 })
+}
 sequelize
     .authenticate ( )
     .then ( () => {
